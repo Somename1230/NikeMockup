ba367f0eb13cde2f0c57ce9f147369e3ab86f138
Somename1230 <99853502+Somename1230@users.noreply.github.com>
2025-06-11 13:14:25 -0400

    Pluh

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import Header from './Components/Header';
-
+import Page1 from './Components/Page1';
 function App() {
 
   return (
     <>
-      <div className="bg-black min-h-screen text-white font-jost text-2xl">
+      <div className="bg-neutral-900 min-h-screen text-white font-jost text-2xl">
 
     <Header/>
-
+    <Page1/>
 
 
       </div>
---
+++ src/Components/Page1.jsx
@@ -0,0 +1,9 @@
+export default function Page1() {
+    return(
+        <div className="flex justify-center pt-20 h-screen">
+            <h1 className="text-10xl text-center">
+                NIKE AIR
+            </h1>
+        </div>
+    )
+}
