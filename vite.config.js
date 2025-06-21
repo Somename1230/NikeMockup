518c5381636d4dcb6f1657d7bf7e8d7767d8e5a9
Somename1230 <99853502+Somename1230@users.noreply.github.com>
2025-06-20 21:46:55 -0400

    tailwind always doin too much

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: process.env.NODE_ENV === "production" ? "/NikeMockup/" : "/",
+  plugins: [react()],
+  base: process.env.VITE_BASE_PATH || '/'
 });
